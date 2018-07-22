c90b4e1273bf55e99e67927b24a7d86c488d4148
user <user@iii-user-PC>
2018-07-22 11:50:49 +0800

    init01

---
+++ java02/src/tw/org/iii/mytest/Hello2.java
@@ -0,0 +1,10 @@
+package tw.org.iii.mytest;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println();
+
+	}
+
+}
---
+++ java02/src/tw/org/iii/mytest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+package tw.org.iii.mytest;
