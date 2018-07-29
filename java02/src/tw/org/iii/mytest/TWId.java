f53bbc7ccad879781faca81d59eceeaade21b803
user <user@iii-user-PC>
2018-07-29 14:35:36 +0800

    1

--- java02/src/tw/org/iii/mytest/TWId.java
+++ java02/src/tw/org/iii/mytest/TWId.java
@@ -22,9 +22,33 @@ public class TWId {
 	//static內容不涉及本類別任何物件的存在(不能直接呼叫下方的m1方法,未建立實體物件)
 	//static方法,只能呼叫static的方法
 	static boolean checkId(String id) {//類別級別擁有的方法,內容不會涉及到this
-	
+	//1.長度為10
+	//2.第一碼為大寫字母
+	//3.第二碼為1或2
+	//4.第三碼後全為數字
+		boolean isRight = false;
 		
-		return true;
+		System.out.println("id長度"+id.length());
+		System.out.println("第5個字元是"+id.charAt(5));
+		
+		if(id.matches("^[A-Z][12][0-9]{8}$")) {//正規表示法判斷身分證字號
+			isRight = true;
+		}
+
+		return isRight;
+		
+//其他寫法...
+//		char[] id2=id.toCharArray();//將String型態的id轉為char array
+//		System.out.println((int)id2[1]);		
+//		if(id.length()==10 && ((int)id2[0]>=65 && (int)id2[0]<=90)
+//			&& ((int)id2[1]==49 || (int)id2[1]==50)) {
+//			return true;
+//		}else {
+//			return false;
+//		}
+		
+
+		
 	}
 	
 	
--- java02/src/tw/org/iii/mytest/java22.java
+++ java02/src/tw/org/iii/mytest/java22.java
@@ -8,10 +8,18 @@ public class java22 {
 //		TWId id1=new TWId();
 //		id1.m1("aaa");//須先建立物件才能實現該方法
 //		
-		if(TWId.checkId("A123456789")) {
-			System.out.println("ok!");
+		if(TWId.checkId("A123456987")) {
+			System.out.println("身分證字號ok!");
 		}else {
-			System.out.println("error!");
+			System.out.println("身分證字號error!");
+		}
+	
+		
+		String b="0910-256966";
+		if(b.matches("^[0][9][0-9]{2}[-][0-9]{6}$")) {//正規表示法判斷手機號碼
+			System.out.println("手機號碼OK");
+		}else {
+			System.out.println("手機號碼XX");
 		}
 
 	}
