3e8f32a014d0c05625e7c5e4879728ba3e897da7
user <user@iii-user-PC>
2018-07-29 10:34:17 +0800

    1

---
+++ java02/src/tw/org/iii/mytest/Bike.java
@@ -0,0 +1,10 @@
+package tw.org.iii.mytest;//代表路徑
+
+public class Bike {//對外宣稱類別,括號內為類別的實作
+	/*
+	 *類別的實作內容(has-a)如下:
+	 *1.靜態:這個類別所擁有的東西:屬性Field
+	 *2.動態:方法(Method)
+	 */
+	
+}
--- java02/src/tw/org/iii/mytest/java_pokerV3.java
+++ java02/src/tw/org/iii/mytest/java_pokerV3.java
@@ -1,47 +1,66 @@
 package tw.org.iii.mytest;
+import java.util.Arrays;
 
 public class java_pokerV3 {
 	public static void main(String[] args) {
 		//洗牌:一維陣列
-		int poker[]=new int[52];//0-12,13-25,25-38,38-51代表四種花色
-		boolean isRepeat;
-		int temp;
-		
-		for(int i=0;i<poker.length;i++) {			
-			do {//至少做一次
-			    temp=(int)(Math.random()*52);
-			
-			//檢查機制
-			isRepeat=false;
-			for(int j=0;j<i;j++) {
-				if(poker[j]==temp) {
-					isRepeat=true;
-					break;
-				}
-			}
-		}while(isRepeat);//若重複則重洗
-		
-		//出上面迴圈代表已經洗到不重複
-		poker[i]=temp;
-		System.out.println(poker[i]);
-		
 		//作業:請用只洗51次的洗牌法(與最後一張交換)
-
 		
+		int[] poker=new int[52];
+		int temp;//抽選第temp張牌,跟最後一張牌交換
+		int poker_temp;//暫存第temp張牌的值
+		
+		for(int i=0;i<poker.length;i++) {
+			poker[i]=i;
+		}//將位置從第0-51張牌的值指定為0-51的值
+		
+		for(int i=0;i<poker.length;i++) {//進行換牌動作
+			temp=(int)(Math.random()*52);
+			poker_temp=poker[temp];
+			poker[temp]=poker[poker.length-i-1];
+			poker[poker.length-i-1]=poker_temp;
+		}
+		
+
+		for(int i=0;i<poker.length;i++) {
+			System.out.println(poker[i]);
+		}
+		
+		System.out.println("----------洗牌完成--------");
+			
 		//發牌給四個人:二維陣列
-		//理牌:照順序
-		//攤牌
+		int[][] players=new int[4][13];//紀錄四位玩家所拿到的牌
+		for(int i=0;i<poker.length;i++) {
+			players[i%4][i/4]=poker[i];
+		}
+		
+		
+		for(int i=0;i<4;i++) {//用for迴圈的另一種寫法
+		    System.out.println("以下為玩家"+(i+1)+"的牌");
+			for(int j=0;j<13;j++) {
+				System.out.print(players[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("----------發牌完成--------");
+		
+		//將數字換成撲克牌的花色和數字
+		String[][] players_2=new String[4][13];
+		
+		String[] sults= {"黑桃","紅心","方塊","梅花"};//花色,分別對應0-12,13-25,26-38,39-51
+		String[] values= {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};//數字
+		
+		//理牌,練習用for each寫法
+		for(int[] b:players) {
+			Arrays.sort(b); 
+			for(int bb:b) {
+				System.out.print(sults[bb/13] + values[bb%13]+" ");
+			}
+			System.out.println();
+		}
 
-		
+		System.out.println("----------理牌完成--------");
 
-		
-		
-		
-		
-		
-		
-		
-		
 		
 	}
 
